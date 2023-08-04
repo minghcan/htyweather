c76ea9d789a98bee793b616023c8b941e72b3735
18064558112 <git@gitee.com:starnetweb/cplus_2023c.git>
2023-08-04 11:21:39 +0800

    加入创建数据库和表的各项操作。

---
+++ app/src/main/java/com/htyweather/android/db/County.java
@@ -0,0 +1,44 @@
+package com.htyweather.android.db;
+
+import org.litepal.crud.LitePalSupport;
+
+public class County extends LitePalSupport {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    private String countryName;
+    private int cityId;
+    private String weatherId;
+
+}
